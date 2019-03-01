b05d3b04475b464ae52f2b0320dc9abd91492d92
Jessý Rún Jónsdóttir <jessy@vr2t-dw448.rhi.hi.is>
2019-03-01 11:54:46 +0000

    main menu

--- Vinnusvæði/MainMenu.py
+++ Vinnusvæði/MainMenu.py
@@ -1,26 +1,34 @@
 import arcade
 import Player
+import Room
 from pick import pick
 
 class MainMenu(arcade.Window):
 
-    #def __init__(self, width, height, title):
+    def __init__(self, width, height, title=""):
 
-    #    file_path = os.path.dirname(os.path.abspath(__file__))
-    #    os.chdir(file_path)
+        file_path = os.path.dirname(os.path.abspath(__file__))
+        os.chdir(file_path)
 
-        # Notum smið yfirklasanns
-        #super().__init__(width, height, title)
+        #Notum smið yfirklasanns
+        super().__init__(width, height, title)
 
-        #self.SCREEN_WIDTH = width
-        #self.SCREEN_HEIGHT = height
+        self.SCREEN_WIDTH = width
+        self.SCREEN_HEIGHT = height
 
-        # Viljum að músin hverfi þegar hún er staðsett yfir glugganum
-        #self.set_mouse_visible(False)
+        #Viljum að músin hverfi þegar hún er staðsett yfir glugganum
+        self.set_mouse_visible(False)
 
         title = 'Choose a player'
         options = ['p1', 'p2', 'p3', 'p4']
         option, index = pick(options, title)
         print(option)
 
-        #self.LEFT_RIGHT_UP_DOWN_key_is_down = [0,0,0,0]
+        self.LEFT_RIGHT_UP_DOWN_key_is_down = [0,0,0,0]
+
+    def on_draw(self):
+    # Köllum á þetta í hvert sinn sem glugginn er opnaður
+        arcade.start_render()
+
+        arcade.draw_texture_rectangle(self.SCREEN_WIDTH // 2, self.SCREEN_HEIGHT // 2,
+                                  self.SCREEN_WIDTH, self.SCREEN_HEIGHT, arcade.load_texture("Images/ModelPack/MakingMap1.png"))
