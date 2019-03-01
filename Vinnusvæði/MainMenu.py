ecb04952a0c9d551ff59333f0d72c2df19325c95
Lovísa Tjorvadottir <lovisarut97@gmail.com>
2019-03-01 14:34:21 +0000

    Main menu skjár

--- Vinnusvæði/MainMenu.py
+++ Vinnusvæði/MainMenu.py
@@ -1,8 +1,8 @@
 import arcade
 import Player
 import Room
-import pick
 import os
+from pick import pick
 
 class MainMenu(arcade.Window):
 
@@ -20,19 +20,58 @@ class MainMenu(arcade.Window):
         #Viljum að músin hverfi þegar hún er staðsett yfir glugganum
         self.set_mouse_visible(False)
 
+#        p1 = arcade.Sprite("Images/Character/P4.png", SPRITE_SCALING)
+#        p2 = arcade.Sprite("Images/Character/P4.png", SPRITE_SCALING)
+#        p3 = arcade.Sprite("Images/Character/P4.png", SPRITE_SCALING)
+#        p4 = arcade.Sprite("Images/Character/P4.png", SPRITE_SCALING)
+
+#        title = 'Choose a player'
+#        options = ['p1', 'p2', 'p3', 'p4']
+#        option, index = pick(options, title)
+#        print(option)
 
         self.LEFT_RIGHT_UP_DOWN_key_is_down = [0,0,0,0]
 
-
-
     def on_draw(self):
-    #Köllum á þetta í hvert sinn sem glugginn er opnaður
+    # Köllum á þetta í hvert sinn sem glugginn er opnaður
         arcade.start_render()
 
         arcade.draw_texture_rectangle(self.SCREEN_WIDTH // 2, self.SCREEN_HEIGHT // 2,
-                                  self.SCREEN_WIDTH, self.SCREEN_HEIGHT, arcade.load_texture("Images/ModelPack/MakingMap1.png"))
+                                 self.SCREEN_WIDTH, self.SCREEN_HEIGHT, arcade.load_texture("Images/Main/Kastali2.PNG"))
 
 
-        character1 = arcade.load_textures("Images/Character/p1.png",[[0,0,31,33]], scale = 3)
-    #    character2 = arcade.load_textures("Images/Character/p2.png",[[0,0,31,33], scale = 3)
-    #    arcade.draw(center_x: float, center_y: float, width: float, height: float, angle: float = 0, alpha: float = 1, transparent: bool = True, repeat_count_x=1, repeat_count_y=1)
+        #Leikmaður 1
+        arcade.draw_texture_rectangle(self.SCREEN_WIDTH // 4, self.SCREEN_HEIGHT // 5,
+                              self.SCREEN_WIDTH//13, self.SCREEN_HEIGHT//10, arcade.load_texture("Images/Character/P1_2.png"))
+
+        #Leikmaður 2
+        arcade.draw_texture_rectangle(self.SCREEN_WIDTH // 1.3333, self.SCREEN_HEIGHT // 5,
+                              self.SCREEN_WIDTH//13, self.SCREEN_HEIGHT//10, arcade.load_texture("Images/Character/P2_2.png"))
+
+        #Highburn
+        arcade.draw_texture_rectangle(self.SCREEN_WIDTH // 2, self.SCREEN_HEIGHT // 1.2,
+                              self.SCREEN_WIDTH//3, self.SCREEN_HEIGHT//10, arcade.load_texture("Images/Main/Highburn.png"))
+
+        #Fortress
+        arcade.draw_texture_rectangle(self.SCREEN_WIDTH // 2, self.SCREEN_HEIGHT // 1.5,
+                              self.SCREEN_WIDTH//3, self.SCREEN_HEIGHT//10, arcade.load_texture("Images/Main/Fortress.png"))
+
+        #Lovísa
+        arcade.draw_texture_rectangle(self.SCREEN_WIDTH // 9, self.SCREEN_HEIGHT // 25,
+                              self.SCREEN_WIDTH//5, self.SCREEN_HEIGHT//30, arcade.load_texture("Images/Main/Lovisa.png"))
+
+        #Jessý
+        arcade.draw_texture_rectangle(self.SCREEN_WIDTH // 2, self.SCREEN_HEIGHT // 25,
+                              self.SCREEN_WIDTH//5, self.SCREEN_HEIGHT//30, arcade.load_texture("Images/Main/Jessy.png"))
+
+        #Davíð
+        arcade.draw_texture_rectangle(self.SCREEN_WIDTH // 1.125, self.SCREEN_HEIGHT // 25,
+                              self.SCREEN_WIDTH//5, self.SCREEN_HEIGHT//30, arcade.load_texture("Images/Main/David.png"))
+
+        #Veldu leikmann
+        arcade.draw_texture_rectangle(self.SCREEN_WIDTH // 2, self.SCREEN_HEIGHT // 2,
+                              self.SCREEN_WIDTH//5, self.SCREEN_HEIGHT//10, arcade.load_texture("Images/Main/Choose.png"))
+
+        #Main menue
+#        arcade.draw_texture_rectangle(self.SCREEN_WIDTH // 10, self.SCREEN_HEIGHT // 10,
+#                              self.SCREEN_WIDTH//10, self.SCREEN_HEIGHT//10, arcade.load_texture("Images/Main/Main.png"))
