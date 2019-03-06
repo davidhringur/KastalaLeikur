31e20eb125bbd95f0ba75a645ea80d6af42f5445
Jessý Rún Jónsdóttir <jessy@vr2t-dw301.rhi.hi.is>
2019-03-06 14:27:35 +0000

    setja inn endakall (dreki)

---
+++ Vinnusvæði/Boss.py
@@ -0,0 +1,14 @@
+import arcade
+
+SCREEN_WIDTH = 1200
+SCREEN_HEIGHT = 600
+
+#Dreki staðsetning á myndum
+        #self.walk_right_textures = arcade.load_textures("Images/Enemy/dreki.png",[[0,64,64,64],[64,64,64,64],[128,64,64,64],[192,64,64,64]], scale = 3)
+        #self.stand_right_textures = arcade.load_textures("Images/Enemy/dreki.png",[[0,64,64,64],[64,64,64,64],[128,64,64,64],[192,64,64,64]], scale = 3)
+        #self.walk_left_textures = arcade.load_textures("Images/Enemy/dreki.png",[[0,0,64,64],[64,0,64,64],[128,0,64,64],[192,0,64,64]], scale = 3)
+        #self.stand_left_textures = arcade.load_textures("Images/Enemy/dreki.png",[[0,0,64,64],[64,0,64,64],[128,0,64,64],[192,0,64,64]], scale = 3)
+        #self.walk_up_textures = arcade.load_textures("Images/Enemy/dreki.png",[[0,192,64,64],[64,192,64,64],[128,192,64,64],[192,192,64,64]], scale = 3)
+        #self.stand_up_textures = arcade.load_textures("Images/Enemy/dreki.png",[[0,192,64,64],[64,192,64,64],[128,192,64,64],[192,192,64,64]], scale = 3)
+        #self.walk_down_textures = arcade.load_textures("Images/Enemy/dreki.png",[[0,128,64,64],[64,128,64,64],[128,128,64,64],[192,128,64,64]], scale = 3)
+        #self.stand_down_textures = arcade.load_textures("Images/Enemy/dreki.png",[[0,128,64,64],[64,128,64,64],[128,128,64,64],[192,128,64,64]], scale = 3)
